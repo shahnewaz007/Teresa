fdf1812e5919e1b04bec64ab5e2962f71e8fe154
Abu Ubaida <abu.ubaida1206@gmail.com>
2019-11-12 18:57:22 +0600

    log in and registration done

--- app/src/main/java/com/ayon/teresa/Base/LoginActivity.java
+++ app/src/main/java/com/ayon/teresa/Base/LoginActivity.java
@@ -1,24 +1,86 @@
 package com.ayon.teresa.Base;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.ayon.teresa.MainActivity;
 import com.ayon.teresa.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
 
     private TextView register_redirect;
+    private FirebaseAuth fAuth;
+    private EditText login_mail;
+    private EditText login_password;
+    private ProgressBar login_progressBar;
+    private Button button_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        login_mail=findViewById(R.id.login_mail);
+        login_password=findViewById(R.id.login_password);
+        login_progressBar=findViewById(R.id.login_progressBar);
+        button_login=findViewById(R.id.button_login);
+        fAuth=FirebaseAuth.getInstance();
         register_redirect = findViewById(R.id.register_redirect);
+
+
+        if(fAuth.getCurrentUser()!=null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        button_login.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                String email=login_mail.getText().toString().trim();
+                String password=login_password.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    login_mail.setError("Email is required!");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    login_password.setError("Password is required!");
+                    return;
+                }
+                if(password.length()<6){
+                    login_password.setError("Password must be contained atleast 6 characters");
+                    return;
+                }
+
+                login_progressBar.setVisibility(View.VISIBLE);
+
+                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Logged in Successfully!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }
+                        else{
+                            Toast.makeText(LoginActivity.this, "Error! "+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
 
         register_redirect.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/ayon/teresa/Base/RegisterActivity.java
+++ app/src/main/java/com/ayon/teresa/Base/RegisterActivity.java
@@ -1,29 +1,105 @@
 package com.ayon.teresa.Base;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.ayon.teresa.MainActivity;
 import com.ayon.teresa.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
     String[] gender = { "Male", "Female"};
     private TextView login_redirect;
-
-
-
+    private Button buttonRegister;
+    private EditText Name;
+    private EditText Age;
+    private Spinner spinner1;
+    private EditText user_phone;
+    private EditText Address;
+    private EditText Guardian_name;
+    private EditText guardian_phone;
+    private EditText Email;
+    private EditText Password;
+    private EditText ConfirmPassword;
+    private FirebaseAuth fAuth;
+    private ProgressBar progressBarRegister;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        buttonRegister=findViewById(R.id.buttonRegister);
+        Name=findViewById(R.id.Name);
+        Age=findViewById(R.id.Age);
+        spinner1=findViewById(R.id.spinner1);
+        user_phone=findViewById(R.id.user_phone);
+        Address=findViewById(R.id.Address);
+        Guardian_name=findViewById(R.id.Guardian_name);
+        guardian_phone=findViewById(R.id.guardian_phone);
+        Email=findViewById(R.id.Email);
+        Password=findViewById(R.id.Password);
+        ConfirmPassword=findViewById(R.id.ConfirmPassword);
+        fAuth=FirebaseAuth.getInstance();
+        progressBarRegister=findViewById(R.id.progressBarRegister);
+
+        mDatabase= FirebaseDatabase.getInstance().getReference();
+
         login_redirect = findViewById(R.id.login_redirect);
+
+
+        buttonRegister.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                String email=Email.getText().toString().trim();
+                String password=Password.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    Email.setError("Email is required!");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    Password.setError("Password is required!");
+                    return;
+                }
+                if(password.length()<6){
+                    Password.setError("Password must be contained atleast 6 characters");
+                    return;
+                }
+
+                 progressBarRegister.setVisibility(View.VISIBLE);
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this, "Registered Successfully!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                        }
+                        else{
+                            Toast.makeText(RegisterActivity.this, "Error! "+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
         login_redirect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
